9c7d93e77cf23319ad24d139b6c0cb9cbdf77ce6
chriscco <2444628489c@gmail.com>
2024-10-24 09:44:50 -0700

    updated

--- co_async/error_handling.hpp
+++ co_async/error_handling.hpp
@@ -12,11 +12,32 @@ std::source_location::current()) {
     }
     return res;
 }
+auto checkErrorNonBlock(auto res, int blockres = 0, int blockerr = EWOULDBLOCK, std::source_location const &loc =
+std::source_location::current()) {
+    if (res == -1) {
+        if (errno != blockerr) [[unlikely]] {
+            throw std::system_error(errno, std::system_category(),
+                                    (std::string)loc.file_name() + ":" +
+                                    std::to_string(loc.line()));
+        }
+        res = blockres;
+    }
+    return res;
+}
 #elif
 /* C语言错误码转换成C++错误码 */
 auto checkError(auto res) {
     if (res == -1) [[unlikely]] {
         throw std::system_error(errno, std::system_category());
+    }
+    return res;
+}
+auto checkErrorNonBlock(auto res, int blockres = 0, int blockerr = EWOULDBLOCK) {
+    if (res == -1) {
+        if (errno != blockerr) [[unlikely]] {
+            throw std::system_error(errno, std::system_category());
+        }
+        res = blockres;
     }
     return res;
 }
