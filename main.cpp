182d369d1f81aa698c5a23621744caaa1b614b66
chriscco <2444628489c@gmail.com>
2024-10-26 22:15:56 -0700

    commented

--- main.cpp
+++ main.cpp
@@ -15,11 +15,25 @@ co_async::EpollLoop epoll_loop;
 /* 处理定时器事件 */
 co_async::TimerLoop timer_loop;
 
+/**
+ * 等待特定文件描述符（fileno）上的 IO 事件
+ * 使用 EpollFileAwaiter 来挂起协程, 直到文件描述符上有数据可读(或事件发生)
+ * events | EPOLLONESHOT 表示这个事件在触发后只会被监听一次
+ * @param loop
+ * @param fileno
+ * @param events
+ * @return
+ */
 inline co_async::Task<void, co_async::EpollFilePromise>
 wait_file(co_async::EpollLoop &loop, int fileno, co_async::EpollEventMask events) {
     co_await co_async::EpollFileAwaiter(loop, fileno, events | EPOLLONESHOT);
 }
 
+/**
+ * 负责从标准输入(文件描述符 0)读取字符
+ * 使用 when_any, 它同时等待两个事件: 从输入读取数据或在 1 秒内超时
+ * @return
+ */
 co_async::Task<std::string> reader() {
     auto which = co_await when_any(wait_file(epoll_loop, 0, EPOLLIN),
                co_async::sleep_for(timer_loop, 1s));
@@ -49,13 +63,23 @@ co_async::Task<void> async_main() {
     }
 }
 
+/**
+ * co_await: 挂起协程, 等待异步操作完成
+ * co_return: 结束协程并返回Task<T>类型
+ * @return
+ */
 int main() {
+    /* 非阻塞模式 */
     int attr = 1;
     ioctl(0, FIONBIO, &attr);
 
     auto t = async_main();
     t.mCoroutine.resume();
     while (!t.mCoroutine.done()) {
+        /**
+         * timer_loop.run() 返回一个可选的延迟, 如果有延迟就传递这个延迟给 epoll_loop.run(ms)
+         * 如果没有延迟，则 epoll_loop.run(-1) 会阻塞, 直到有 I/O 事件发生
+         */
         if (auto delay = timer_loop.run()) {
             auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(*delay).count();
             epoll_loop.run(ms);
